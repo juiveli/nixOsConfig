a2ff20e7290a6d9818f88732dbce4d093cf4b113
juiveli <j00nas.makinen@hotmail.com>
2025-01-17 00:55:35 +0200

    added steam to main-pc

--- profiles/main-pc/configuration.nix
+++ profiles/main-pc/configuration.nix
@@ -19,4 +19,14 @@
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = false;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall =
+      true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall =
+      true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall =
+      true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
