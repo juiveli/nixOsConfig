b83a775303c0daf6f90901fbe476e7e4e91cd14e
juiveli <j00nas.makinen@hotmail.com>
2025-01-17 00:50:37 +0200

    added steam to test pc

--- profiles/nixos-test/configuration.nix
+++ profiles/nixos-test/configuration.nix
@@ -15,4 +15,14 @@
 
   system.autoUpgrade.enable = false;
   system.autoUpgrade.allowReboot = false;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall =
+      true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall =
+      true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall =
+      true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
