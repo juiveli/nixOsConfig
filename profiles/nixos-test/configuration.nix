68c0047e49ab774a2f4966a14aa31378fee8e328
juiveli <j00nas.makinen@hotmail.com>
2025-01-25 12:09:18 +0200

    Added nix-flatpak to nixos-test

--- profiles/nixos-test/configuration.nix
+++ profiles/nixos-test/configuration.nix
@@ -4,17 +4,26 @@
 
 { config, pkgs, ... }:
 
-{
+let
+  # version 0.5.2
+  nix-flatpak = (builtins.getFlake
+    "github:gmodena/nix-flatpak/8bdc2540da516006d07b04019eb57ae0781a04b3");
+
+in {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../../common-configurations/configuration.nix
     ../../common-configurations/nvidia-drivers.nix
+    nix-flatpak.nixosModules.nix-flatpak
   ];
+
   networking.hostName = "nixos-test"; # Define your hostname.
   services.nvidia-drivers.enable = true;
 
   system.autoUpgrade.enable = false;
   system.autoUpgrade.allowReboot = false;
+
+  services.flatpak.enable = true;
 
   programs.steam = {
     enable = true;
