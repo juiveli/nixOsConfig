ae80ef258f28aa3f8470452ca7af6fef65c7f019
joonas <j00nas.makinen@hotmail.com>
2025-04-04 14:22:43 +0300

    Added existing quadlets

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    quadlet-nix = {
+      url = "github:SEIAROTg/quadlet-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    testServer = {
+      url = "./testServer";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    caddy = {
+      url = "./caddy";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      quadlet-nix,
+      testServer,
+      caddy,
+      ...
+    }@attrs:
+    {
+    
+      nixosModules = { 
+        quadlet-collection = {
+            imports = [ quadlet-nix.nixosModules.quadlet ];
+        };
+      };    
+    
+      homeManagerModules = {
+        quadlet-collection =
+          { config, pkgs, ... }:
+
+          {
+
+            imports = [
+              quadlet-nix.homeManagerModules.quadlet
+              testServer.nixosModules.quadlet
+              caddy.nixosModules.quadlet
+            ];
+
+          };
+      };
+    };
+}
---
+++ testServer/flake.nix
@@ -0,0 +1,28 @@
+# Nix-podman-testServer-quadlet
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { nixpkgs, ... } @attrs: {
+    nixosModules.quadlet = { config, lib, pkgs, ... }: 
+    {
+        config = {
+          systemd.user.startServices = "sd-switch";
+
+          virtualisation.quadlet.containers = {
+            testServer = {
+              autoStart = true;
+              serviceConfig = {
+                RestartSec = "10";
+                Restart = "always";
+              };
+              containerConfig = {
+                image = "quay.io/libpod/banner:latest";
+                publishPorts = [ "8001:80" ];
+              };
+            };
+          };
+        };
+      };
+  };
+}
