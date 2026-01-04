61fb5825bdf74938ee023776998d690bcbf13fdc
joonas <j00nas.makinen@hotmail.com>
2026-01-05 00:45:34 +0200

    changed version to nix 25.11

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   inputs = {
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     nix-dev-toolkit = {
       url = "github:juiveli/nix-dev-toolkit";
